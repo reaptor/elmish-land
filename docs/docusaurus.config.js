3e94033187ca957d5a9321c4bf4b01fae908c826
Kristofer Löfberg <kristofer.lofberg@svea.com>
2023-12-05 18:25:22 +0100

    Default dark mode for docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -65,6 +65,7 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      defaultMode: 'dark',
       navbar: {
         title: 'Elmish Land',
         logo: {
