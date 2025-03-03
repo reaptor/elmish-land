09bdedf05252df8bb092451ceaf83530214cafe9
klofberg <kristofer@reaptor.com>
2025-03-03 10:08:17 +0100

    Update introducing elmish land post

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -47,6 +47,8 @@ const config = {
                     //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 blog: {
+                    blogSidebarTitle: 'All posts',
+                    blogSidebarCount: 'ALL',
                     showReadingTime: true,
                     feedOptions: {
                         type: ['rss', 'atom'],
